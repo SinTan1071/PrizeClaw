378073d754c9b356393acdd0b6a72072129a5d9c
SinTan1071 <297085213@qq.com>
2017-12-12 15:31:35 +0800

    新增

--- controllers/wechatCtr.js
+++ controllers/wechatCtr.js
@@ -72,7 +72,7 @@ exports.oauthWechat = async(ctx, next) => {
         if (wechat_userinfo && wechat_userinfo.openid){
         var user = await userService.getUserByWechatId(wechat_userinfo.openid)
         console.log("获取到user", user)
-        if (!user.wechat_openid)
+        if (!user || !user.wechat_openid)
             var new_user = await userService.createWechatUser(wechat_userinfo)
             if (new_user.wechat_openid){
                 ctx.redirect(CONF.index_page + "?data=" + (new Buffer(JSON.stringify(new_user)).toString('base64')))
